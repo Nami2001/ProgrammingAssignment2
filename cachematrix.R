6af58ce7c0b3cb067420cbcc67b6bba985064367
Nami2001 <nnrashinkar@mymail.mapua.edu.ph>
2021-04-18 19:47:43 +0800

    Revert "Update cachematrix.R"
    This reverts commit 9b26ce711139df878951db3e897301b02064c0e6.

--- cachematrix.R
+++ cachematrix.R
@@ -1,29 +1,15 @@
+## Put comments here that give an overall description of what your
+## functions do
 
-## 2 type functions: makeCacheMatrix, cacheSolve
-## makeCacheMatrix: set, setinv, get, getinv in it
+## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()){
-  inv<-NULL #to initialize inverse as NULL term
-  set<- function(y){
-    x<<-y
-    inv<<-NULL
-  }
-  get<-function()x
-  setinverse<-function(inverse)
-    getinverse<-function(){inv}
-  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
+makeCacheMatrix <- function(x = matrix()) {
+
 }
 
 
-##used to get the cache data needed
+## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) ## cache data
-{}
-inv<-x$getinverse()
-if(!is.null(inv)){ ##to verify null output inverse
-  message("getting the value of cache data")
-  return(inv) 
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
 }
-value<- x$get()
-x$setinv(inv)
-inv ##returns a value matrix that is the inverse of the value 'x'
